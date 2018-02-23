f1225fffb7fbb852e6ee6b24a61854abe425c079
monerador <andre@mosin.com.br>
2018-02-22 21:27:36 -0800

    update to v 3.1.2.0

--- b3coind_install.sh
+++ b3coind_install.sh
@@ -21,9 +21,9 @@ apt-get upgrade -y
 apt-get install -y libssl-dev libdb++-dev libboost-all-dev libqrencode-dev unzip wget
 
 # Install b3coind
-wget https://github.com/amosin/b3coind_install/raw/master/b3coind-3.1.1.2.deb -O /var/cache/apt/archives/b3coind-3.1.1.2.deb
+wget https://github.com/amosin/b3coind_install/raw/master/b3coind-3.1.2.0.deb -O /var/cache/apt/archives/b3coind-3.1.2.0.deb
 
-apt-get install /var/cache/apt/archives/b3coind-3.1.1.2.deb
+apt-get install /var/cache/apt/archives/b3coind-3.1.2.0.deb
 
 
 # Swap
@@ -39,7 +39,7 @@ su -c "cat > ~/.B3-CoinV2/b3coin.conf << EOF
 rpcuser=b3coinrpc
 rpcpassword=`openssl rand -base64 32`
 EOF" -s /bin/sh b3fn01
-su -c "wget https://github.com/B3-Coin/B3-CoinV2/releases/download/v3.1.1.2/b3bootstrap.zip -O /home/b3fn01/.B3-CoinV2/b3bootstrap.zip" -s /bin/sh b3fn01
+su -c "wget https://github.com/B3-Coin/B3-CoinV2/releases/download/v3.1.2.0/b3bootstrap.zip -O /home/b3fn01/.B3-CoinV2/b3bootstrap.zip" -s /bin/sh b3fn01
 su -c "unzip ~/.B3-CoinV2/b3bootstrap.zip -d ~/.B3-CoinV2/ && rm ~/.B3-CoinV2/b3bootstrap.zip" -s /bin/sh b3fn01
 su -c "b3coind" -s /bin/sh b3fn01 &
 su - b3fn01
